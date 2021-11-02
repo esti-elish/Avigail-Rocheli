bad0aa1e8bbe84499e08c92f94fa7272adccacba
רחל אוסטטשר <327093159@mbydomain>
2021-11-02 19:49:22 +0200

    Hello2

---
+++ startup/src/main/java/com/example/springboot/textUtils.java
@@ -0,0 +1,7 @@
+public class textUtils{
+	public static String getText(){
+		String [] array = new String[]{"we","used","stringUtils","library"};
+		String result = StringUtils.concat(array);
+		return result;
+	}
+}
