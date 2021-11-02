ecb694b6d29303fc0954479e9bc0fb582f34ea0b
רחל אוסטטשר <327093159@mbydomain>
2021-11-02 20:00:17 +0200

    hi1

--- startup/src/main/java/com/example/springboot/textUtils.java
+++ startup/src/main/java/com/example/springboot/textUtils.java
@@ -1,3 +1,7 @@
+package com.example.springboot;
+
+import com.sun.tools.javac.util.StringUtils;
+
 public class textUtils{
 	public static String getText(){
 		String [] array = new String[]{"we","used","stringUtils","library"};
